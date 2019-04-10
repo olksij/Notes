ac66ec9b9bbe38a71eb9a050780934354567b979
Alex Besida <pixel.coder7@gmail.com>
2019-04-10 19:34:53 +0300

    PWA test #11

--- sw.js
+++ sw.js
@@ -22,9 +22,9 @@ self.addEventListener('install', async e => {
   return self.skipWaiting();
 });
 
-/*self.addEventListener('activate', e => {
+self.addEventListener('activate', e => {
   self.clients.claim();
-});*/
+});
 
 self.addEventListener('fetch', async e => {
   const req = e.request;
