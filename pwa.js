395f81e2cf9e568913bcd7af10735ac26cc03576
Alex Besida <pixel.coder7@gmail.com>
2019-04-20 22:50:06 +0300

    Test

--- pwa.js
+++ pwa.js
@@ -1,3 +1,21 @@
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  btnAdd.style.display = 'block';
+
+  console.log("Allowed");
+});
+
+window.addEventListener('appinstalled', (evt) => {
+  app.logEvent('a2hs', 'installed');
+});
+
+
 if (!Cache.prototype.add) {
   Cache.prototype.add = function add(request) {
     return this.addAll([request]);
