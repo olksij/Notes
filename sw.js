64db21cb739fed676ac1b220b261aa0a2f5da467
Alex Besida <pixel.coder7@gmail.com>
2019-04-20 16:56:49 +0300

    PWA test #17

--- sw.js
+++ sw.js
@@ -1,61 +1,52 @@
-const cacheName = 'f-notes-v1';
+// Chrome's currently missing some useful cache methods,
+// this polyfill adds them.
+importScripts('sw-cache-polyfill.js');
 
+// Here comes the install event!
+// This only happens once, when the browser sees this
+// version of the ServiceWorker for the first time.
 self.addEventListener('install', function(event) {
+  // We pass a promise to event.waitUntil to signal how 
+  // long install takes, and if it failed
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(
-        [
-          './',
-          './index.html',
-          './FluxUI.css',
-          './main.css',
-          './main.js',
-          './Assets/AppIcon.ico',
-          './Assets/AppIcon.png',
-          './Assets/AppIcon512.png',
-          './Assets/AppIconWhite.svg',
-          './Fonts/GoogleSans-Medium.ttf',
-          './Fonts/GoogleSans-Regular.ttf',
-          './Fonts/GoogleSans-Bold.ttf',
-          './JS/buttons.js',
-          './JS/firestore.js'
-        ]
-      );
+    // We open a cache…
+    caches.open('sw-notes-v1.0.1').then(function(cache) {
+      // And add resources to it
+      return cache.addAll([
+        './',
+        './index.html',
+        './FluxUI.css',
+        './main.css',
+        './main.js',
+        './Assets/AppIcon.ico',
+        './Assets/AppIcon.png',
+        './Assets/AppIcon512.png',
+        './Assets/AppIconWhite.svg',
+        './Fonts/GoogleSans-Medium.ttf',
+        './Fonts/GoogleSans-Regular.ttf',
+        './Fonts/GoogleSans-Bold.ttf',
+        './JS/buttons.js',
+        './JS/firestore.js'
+      ]);
     })
   );
 });
 
-self.addEventListener('activate', e => {
-  self.clients.claim();
-});
-
+// The fetch event happens for the page request with the
+// ServiceWorker's scope, and any request made within that
+// page
 self.addEventListener('fetch', function(event) {
+  // Calling event.respondWith means we're in charge
+  // of providing the response. We pass in a promise
+  // that resolves with a response object
   event.respondWith(
-    caches.open(cacheName).then(function(cache) {
-      return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function(response) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
+    // First we look for something in the caches that
+    // matches the request
+    caches.match(event.request).then(function(response) {
+      // If we get something, we return it, otherwise
+      // it's null, and we'll pass the request to
+      // fetch, which will use the network.
+      return response || fetch(event.request);
     })
   );
 });
-
-async function cacheFirst(req) {
-  const cache = await caches.open(cacheName);
-  const cached = await cache.match(req);
-  return cached || fetch(req);
-}
-
-async function networkAndCache(req) {
-  const cache = await caches.open(cacheName);
-  try {
-    const fresh = await fetch(req);
-    await cache.put(req, fresh.clone());
-    return fresh;
-  } catch (e) {
-    const cached = await cache.match(req);
-    return cached;
-  }
-}
